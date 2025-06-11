e0547cec01637dd092a139d3f069ca46d5a966f3
Nico <gruwe.nicolas@hotmail.fr>
2025-06-11 18:50:37 +0700

    fix: Correction des métadonnées de la page About - Séparation du client component

---
+++ app/about/metadata.ts
@@ -0,0 +1,12 @@
+export const metadata = {
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.nicolas-gruwe.fr/img/about3.webp',
+        width: 1200,
+        height: 630,
+        alt: 'À propos de Nicolas Gruwe',
+      },
+    ],
+  },
+} 
